4e263856bc1acd70b20dfe83fef5858100c7880c
Sam <samnewell91@gmail.com>
2024-01-12 15:19:15 +0000

    Begun to style "hamburger" menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
     <nav>
       <h2>Boss Burgers</h2>
       <div>
-        <ul>
+        <ul className={navOpen ? "show-nav" : ""}>
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -17,6 +21,16 @@ export default function Navbar() {
           </li>
         </ul>
       </div>
+      <button
+        className="nav-button"
+        onClick={() => setNavOpen((open) => !open)}
+      >
+        <div className="button-top"></div>
+        <div
+          className={navOpen ? "button-middle button-open" : "button-middle"}
+        ></div>
+        <div className="button-bottom"></div>
+      </button>
     </nav>
   );
 }
